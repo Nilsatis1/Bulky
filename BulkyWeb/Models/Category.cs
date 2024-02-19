98235dad1108989add40d31948d01abc37f9b38e
Scott Jackson <nilsatis1@hotmail.com>
2024-02-19 19:09:49 +0000

    39 Tag Helpers

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace BulkyWeb.Models
 {
@@ -7,7 +8,9 @@ namespace BulkyWeb.Models
 		[Key]
 		public int Id { get; set; }
 		[Required]
+		[DisplayName("Category Name")]
 		public string Name { get; set; }
+		[DisplayName("Display Order")]
 		public int DisplayOrder { get; set; }
     }
 }
