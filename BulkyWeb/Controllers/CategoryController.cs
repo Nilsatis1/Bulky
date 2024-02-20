e86c83bd791ba561963d19006e24eb21dfa2abb6
Scott Jackson <nilsatis1@hotmail.com>
2024-02-20 15:25:42 +0000

    44 Client side validation

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -22,5 +22,20 @@ namespace BulkyWeb.Controllers
 		{
 			return View();
 		}
+		[HttpPost]
+		public IActionResult Create(Category obj)
+		{
+			if (obj.Name == obj.DisplayOrder.ToString())
+			{
+				ModelState.AddModelError("name", "The Display Order cannot exactly match the name");
+			}
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Add(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
 	}
 }
