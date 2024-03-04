b3ada059c84867fa524f7c8f51167f37c34ecb1e
Scott Jackson <nilsatis1@hotmail.com>
2024-03-04 18:01:29 +0000

    Created Image upload functionality

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -12,9 +12,11 @@ namespace BulkyWeb.Areas.Admin.Controllers
 	public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -57,6 +59,18 @@ namespace BulkyWeb.Areas.Admin.Controllers
             
             if (ModelState.IsValid)
             {
+                String wwwRootPath = _webHostEnvironment.WebRootPath;
+                if(file != null) 
+                {
+                    String filename = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    String productPath = Path.Combine(wwwRootPath, @"images\product");
+
+					using (var fileStream = new FileStream(Path.Combine(productPath, filename),FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                    productVM.Product.ImageUrl =@"\Images\product\" + filename;
+                }
                 _unitOfWork.Product.Add(productVM.Product);
                 _unitOfWork.Save();
                 TempData["success"] = "Product created successfully";
